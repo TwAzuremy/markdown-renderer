dd9c50db0ce2d27f27f28594f7e1e441c6020299
Azuremy <958559114@qq.com>
2025-04-23 21:59:28 +0800

    test (Restorer): Comparison of test restoration results with pretreatment.

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,15 +1,15 @@
 import "./css/markdown-editor.scss";
 
 import { memo, useEffect, useRef, useState } from "react";
-import ContentEditable from 'react-contenteditable';
 import { Markdown } from "../processor/renderer/Renderer";
+import { HTML } from "../processor/restorer/Restorer";
 
 const MarkdownEditor = memo(({ src }) => {
     const editorRef = useRef(null);
     const [content, setContent] = useState(null);
 
     useEffect(() => {
-        (async () => {
+        ; (async () => {
             const parts = await Markdown.parse(src);
             const accumulatedContent = parts.join("");
 
@@ -17,13 +17,23 @@ const MarkdownEditor = memo(({ src }) => {
         })();
     }, [src]);
 
+    // TODO [TEST] Used only for test data, this code will be deleted when the test is completed.
+    // Note: This uses a null judgment, and removes this code if you want to test rendering null data.
+    useEffect(() => {
+        if (content) {
+            ; (async () => {
+                const src = await HTML.parse(editorRef.current);
+                console.log(src);
+            })();
+        }
+    }, [content]);
+
     return (
-        <ContentEditable
+        <div
             className={"markdown-editor"}
-            innerRef={editorRef}
-            html={content}
-            tagName={"div"}
-        />
+            ref={editorRef}
+            contentEditable
+            dangerouslySetInnerHTML={{ __html: content }}></div>
     );
 });
 
