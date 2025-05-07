9d483ff7ccb0ec7363ba92c28df95f3b6c938fee
Azuremy <958559114@qq.com>
2025-05-07 16:36:27 +0800

    docs: Add document comments to the restorer.

--- src/processor/restorer/Restorer.js
+++ src/processor/restorer/Restorer.js
@@ -2,9 +2,15 @@ import { elementParser } from "./ElementParser";
 
 export class HTML {
     /**
+     * Revert the HTML DOM to a markdown list of paragraph strings split by paragraph.
+     * If the editor itself is passed, then the child elements are traversed for parsing, otherwise it is parsed directly.
+     * 
      * @param {HTMLElement} element 
+     * @returns {[String]} 
      */
     static async parse(element) {
+        if (!element.classList.contains("markdown-editor")) return [this.parseParagraph(element)];
+
         const paragraphs = Array.from(element.children);
 
         const result = paragraphs.map(paragraph => {
@@ -14,6 +20,12 @@ export class HTML {
         return result;
     }
 
+    /**
+     * Parsing paragraphs.
+     * 
+     * @param {HTMLElement} paragraphElement 
+     * @returns {string} markdown paragraph string.
+     */
     static parseParagraph(paragraphElement) {
         const type = paragraphElement.dataset.type;
 
