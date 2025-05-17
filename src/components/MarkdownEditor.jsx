3582756c7cd0802fb6d4f31596333a278be7f6aa
Azuremy <958559114@qq.com>
2025-05-17 19:24:29 +0800

    test ( Restorer ): Remove test-related code.

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -2,38 +2,28 @@ import "./css/markdown-editor.scss";
 
 import { memo, useEffect, useRef, useState } from "react";
 import { Markdown } from "../processor/renderer/Renderer";
-import { HTML } from "../processor/restorer/Restorer";
 
 const MarkdownEditor = memo(({ src }) => {
     const editorRef = useRef(null);
-    const [content, setContent] = useState(null);
+    const [paragraphs, setParagraphs] = useState("");
 
     useEffect(() => {
         ; (async () => {
             const parts = await Markdown.parse(src);
             const accumulatedContent = parts.join("");
 
-            requestAnimationFrame(() => setContent(accumulatedContent));
+            requestAnimationFrame(() => setParagraphs(accumulatedContent));
         })();
     }, [src]);
-
-    // TODO [TEST] Used only for test data, this code will be deleted when the test is completed.
-    // Note: This uses a null judgment, and removes this code if you want to test rendering null data.
-    useEffect(() => {
-        if (content) {
-            ; (async () => {
-                const src = await HTML.parse(editorRef.current);
-                console.log(src);
-            })();
-        }
-    }, [content]);
 
     return (
         <div
             className={"markdown-editor"}
             ref={editorRef}
-            contentEditable
-            dangerouslySetInnerHTML={{ __html: content }}></div>
+            dangerouslySetInnerHTML={{ __html: paragraphs }}
+            suppressContentEditableWarning
+            contentEditable>
+        </div>
     );
 });
 
