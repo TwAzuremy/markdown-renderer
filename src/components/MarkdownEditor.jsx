f44f6947a7f13b3942df8fdd86aa06a763fd5695
Azuremy <958559114@qq.com>
2025-05-18 08:28:28 +0800

    feat (Renderer): Change the rendering method to tiled rendering.
    - Traditional rendering replaces chunk rendering.
    - Add Loading and Error to the page.
    - Add components that can be customized for Loading and Error.

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,28 +1,81 @@
 import "./css/markdown-editor.scss";
 
-import { memo, useEffect, useRef, useState } from "react";
+import { Fragment, memo, useEffect, useRef, useState } from "react";
+import parse from 'html-react-parser';
 import { Markdown } from "../processor/renderer/Renderer";
 
-const MarkdownEditor = memo(({ src }) => {
+const MarkdownEditor = memo(({
+    src,
+    loadingComponent,
+    errorComponent,
+    settings = { chunkSize: 5 }
+}) => {
     const editorRef = useRef(null);
-    const [paragraphs, setParagraphs] = useState("");
+
+    const [chunks, setChunks] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
-        ; (async () => {
-            const parts = await Markdown.parse(src);
-            const accumulatedContent = parts.join("");
+        let isMounted = true;
 
-            requestAnimationFrame(() => setParagraphs(accumulatedContent));
-        })();
+        const renderChunks = async () => {
+            try {
+                const parts = await Markdown.parse(src);
+
+                const renderNextChunk = (index = 0) => {
+                    // If the component is unmounted or we've reached the end of the chunks, stop the process
+                    if (!isMounted || index >= parts.length) {
+                        setIsLoading(false);
+                        return;
+                    }
+
+                    // Determine the end index for the current chunk
+                    const endIndex = Math.min(index + settings.chunkSize, parts.length);
+
+                    // Append the current chunk to the chunks state
+                    setChunks(prev => [...prev, ...parts.slice(index, endIndex)]);
+                    // Request the next animation frame to continue rendering the next chunk
+                    requestAnimationFrame(() => renderNextChunk(endIndex));
+                }
+
+                renderNextChunk();
+            } catch (error) {
+                if (isMounted) {
+                    setIsLoading(false);
+                    setIsError(true);
+                }
+            }
+        }
+
+        renderChunks();
+
+        // Cleanup function to set the isMounted flag to false when the component is unmounted
+        return () => {
+            isMounted = false;
+        }
     }, [src]);
 
     return (
         <div
             className={"markdown-editor"}
             ref={editorRef}
-            dangerouslySetInnerHTML={{ __html: paragraphs }}
             suppressContentEditableWarning
-            contentEditable>
+            contentEditable={!isLoading}>
+            {/* TODO: The more sloppy Loading and Error forms will be redesigned in the future. */}
+            {isLoading &&
+                <div className={"markdown-renderer__loading"}>
+                    {loadingComponent || <p>Loading...</p>}
+                </div>}
+            {isError &&
+                <div className={"markdown-renderer__error"}>
+                    {errorComponent || <p>Error loading content</p>}
+                </div>}
+            {chunks.map((html, index) => (
+                <Fragment key={index}>
+                    {parse(html)}
+                </Fragment>
+            ))}
         </div>
     );
 });
